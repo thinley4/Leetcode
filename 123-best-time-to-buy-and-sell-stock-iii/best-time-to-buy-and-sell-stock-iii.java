5ef38cc1bcb736e88bb04527a9e2cc71daa2a7af
Thinley <128626751+thinley4@users.noreply.github.com>
2025-01-15 13:24:44 +0530

    Time: 143 ms (12.29%) | Memory: 80 MB (17.14%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -2,22 +2,33 @@ import java.util.Arrays;
 
 class Solution {
     private int solve(int index, boolean buy, int cnt, int n, int[] prices, int[][][] dp) {
+        // Base case
+        // n = length of prices
+        // cnt = 2 (Max transistion)
         if (index == n || cnt == 0) {
             return 0;
         }
+        // buy = 1, sell = 0
         if (dp[index][buy ? 1 : 0][cnt] != -1) {
             return dp[index][buy ? 1 : 0][cnt];
         }
         int profit = 0;
+
         if (buy) {
+            // If I want to buy
+            // Money will be gone so '-prices[index]'
             int pick = -prices[index] + solve(index + 1, false, cnt, n, prices, dp);
+            // If I don't want to buy
             int notPick = solve(index + 1, true, cnt, n, prices, dp);
             profit = Math.max(pick, notPick);
         } else {
+            // If I want to sell
             int pick = prices[index] + solve(index + 1, true, cnt - 1, n, prices, dp);
+            // If I don't want to sell
             int notPick = solve(index + 1, false, cnt, n, prices, dp);
             profit = Math.max(pick, notPick);
         }
+        
         return dp[index][buy ? 1 : 0][cnt] = profit;
     }
 
