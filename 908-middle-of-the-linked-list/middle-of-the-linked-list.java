cc3a182e1c913d151892fe8de9d431f3ab42d544
Thinley <128626751+thinley4@users.noreply.github.com>
2025-01-20 23:01:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (21.52%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -12,7 +12,12 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-
+        // Fast Node is moving by twice '2'
+        // slow Node is moving by '1' step
+        
+        // It perfectly work for 'odd' number of Nodes
+        // But inorder for it to work for 'even' no. of Nodes
+        // 'fast.next != null' concept come into play.
         while(fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
