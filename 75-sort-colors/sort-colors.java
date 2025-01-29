f44bba3927ea35a247151848d7bdc77b79ab3742
Thinley <128626751+thinley4@users.noreply.github.com>
2025-01-29 10:02:25 +0530

    Time: 1 ms (21.53%) | Memory: 42.2 MB (30.04%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        HashMap<Integer, Integer> count = new HashMap<>();
+        // Initialize freq of 0, 1 & 2 as '0'.
+        count.put(0, 0);
+        count.put(1, 0);
+        count.put(2, 0);
+        // Store the num with freq in hashmap
+        for(int num : nums) {
+            count.put(num, count.get(num) + 1);
+        }
+        int idx = 0;
+        // int color = 0, 1, 2 (red, white, blue)
+        for(int color = 0; color < 3; color++) {
+            int freq = count.get(color);
+            for(int j=0; j<freq; j++) {
+                nums[idx] = color;
+                idx++;
+            }
+        }
     }
 }
