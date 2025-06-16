4b2fa376f0e38f53d3408bfea90fa321ca8e2fa4
Thinley <128626751+thinley4@users.noreply.github.com>
2025-06-16 21:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 51.1 MB (5.92%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            // mid is odd, make to even
+            if(mid % 2 == 1) 
+                mid--;
+            // mid always in even index
+            // so, left & right sides are always even
+            if(nums[mid] != nums[mid+1]) {
+                // if mid not equals to (mid+1) 
+                // right side is even
+                // so, non-duplicate is at left side
+                right = mid;
+            } else {
+                // if mid equals to (mid+1)
+                // right side will be odd
+                // Y can take eg and check
+                left = mid + 2;
+            }
+        }
+        return nums[right];
+    }
+}
