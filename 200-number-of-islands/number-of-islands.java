359761f757da210faf0504dce5a11a6c70bc1ffa
Thinley <128626751+thinley4@users.noreply.github.com>
2025-07-06 16:36:42 +0530

    Time: 5 ms (34.03%) | Memory: 52.2 MB (19.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,44 +1,35 @@
 class Solution {
+    public void findLands(boolean[][] visited, char[][] grid, int r, int c, int rows, int cols) {
+        // left, right, up, down
+        int[][] directions = {{0,-1}, {0, 1}, {-1, 0}, {1, 0}}; 
+
+        for(int[] direction : directions) {
+            int crow = r + direction[0];
+            int ccol = c + direction[1];
+            // // valid - inside grid, not visited, char = '1'
+            if(crow >= 0 && crow < rows && ccol >= 0 && ccol < cols && !visited[crow][ccol] && grid[crow][ccol] == '1') {
+                visited[crow][ccol] = true; // make true
+                findLands(visited, grid, crow, ccol, rows, cols);
+            }
+        }
+    }
+
     public int numIslands(char[][] grid) {
-        HashSet<String> visited = new HashSet<>();
-        int island = 0;
         int rows = grid.length;
         int cols = grid[0].length;
-
-        int[][] directions = {{1,0}, {-1,0}, {0, 1}, {0, -1}};
+        boolean[][] visited = new boolean[rows][cols];
+        int land = 0;
 
         for(int r=0; r<rows; r++) {
             for(int c=0; c<cols; c++) {
-                // not visited & is '1'
-                if(!visited.contains(r+","+c) && grid[r][c] == '1'){
-                    island++;
-                    bfs(rows, cols, r, c, grid, directions, visited);
+                // 1 and not visited
+                if(grid[r][c] == '1' && !visited[r][c]) {
+                    land++;
+                    visited[r][c] = true;
+                    findLands(visited, grid, r, c, rows, cols);
                 }
             }
         }
-        return island;
-    }
-    public void bfs(int rows, int cols, int r, int c, char[][] grid, int[][] directions, HashSet<String> visited) {
-        Queue<int[]> q = new LinkedList<>();
-        // store in visited & queue
-        visited.add(r+","+c);
-        q.add(new int[] {r, c});
-
-        while(!q.isEmpty()) {
-            int[] point = q.poll();
-            int row = point[0];
-            int col = point[1];
-
-            for(int[] direction : directions) {
-                // Updated row = nr & col = nc 
-                // (left, right, down, up)
-                int nr = row + direction[0];
-                int nc = col + direction[1];
-                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && !visited.contains(nr +"," + nc)) {
-                    q.add(new int[] {nr, nc});
-                    visited.add(nr+","+ nc);
-                }
-            }
-        }
+        return land;
     }
 }
