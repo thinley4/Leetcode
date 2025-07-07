7cef02eb6ec504b031cde39d01c01f1a0e990862
Thinley <128626751+thinley4@users.noreply.github.com>
2025-07-07 21:06:42 +0530

    Time: 6 ms (24.71%) | Memory: 52.2 MB (22.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,35 +1,48 @@
 class Solution {
-    public void findLands(boolean[][] visited, char[][] grid, int r, int c, int rows, int cols) {
+    public void findLands(int r, int c, boolean[][] visited, int rlen, int clen, char[][] grid, Queue<int[]> q) {
         // left, right, up, down
-        int[][] directions = {{0,-1}, {0, 1}, {-1, 0}, {1, 0}}; 
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        for(int[] direction : directions) {
-            int crow = r + direction[0];
-            int ccol = c + direction[1];
-            // // valid - inside grid, not visited, char = '1'
-            if(crow >= 0 && crow < rows && ccol >= 0 && ccol < cols && !visited[crow][ccol] && grid[crow][ccol] == '1') {
-                visited[crow][ccol] = true; // make true
-                findLands(visited, grid, crow, ccol, rows, cols);
+        while(!q.isEmpty()) {
+            int size = q.size();
+
+            if(size > 0) {
+                int[] currElement = q.poll();
+                // check all 4 directions
+                for(int[] direction : directions) {
+                    int nr = currElement[0] + direction[0];
+                    int nc = currElement[1] + direction[1];
+                    // Valid: Inside grid & 1 & not visited
+                    if(nr >= 0 && nr < rlen && nc >= 0 && nc < clen && !visited[nr][nc] && grid[nr][nc] == '1') {
+                        visited[nr][nc] = true;
+                        q.add(new int[] {nr, nc});
+                    } 
+                }
+                size--;
             }
         }
     }
 
     public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        boolean[][] visited = new boolean[rows][cols];
-        int land = 0;
+        int count = 0;
+        int rlen = grid.length;
+        int clen = grid[0].length;
+        boolean[][] visited;
+        visited = new boolean[rlen][clen];
+        Queue<int[]> q = new LinkedList<>();
 
-        for(int r=0; r<rows; r++) {
-            for(int c=0; c<cols; c++) {
-                // 1 and not visited
-                if(grid[r][c] == '1' && !visited[r][c]) {
-                    land++;
-                    visited[r][c] = true;
-                    findLands(visited, grid, r, c, rows, cols);
+        // visite all elements in a grid
+        for(int i=0; i < rlen; i++) {
+            for(int j=0; j < clen; j++) {
+                // if island '1' & not visited
+                if(grid[i][j] == '1' && !visited[i][j]) {
+                    count++;
+                    visited[i][j] = true;
+                    q.add(new int[]{i, j});
+                    findLands(i, j, visited, rlen, clen, grid, q);
                 }
             }
         }
-        return land;
+        return count;
     }
 }
