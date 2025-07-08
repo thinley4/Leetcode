08473688dc99c826ec94e91660d8ba2233adbfa9
Thinley <128626751+thinley4@users.noreply.github.com>
2025-07-08 16:02:48 +0530

    Time: 5 ms (33.91%) | Memory: 52.6 MB (10.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,17 +1,26 @@
+// Algorithm
+// 1. visiting each and every elements 
+// 2. check island or water
+// 3. if (land and not visited)
+// - increment
+// - find conneted island (visit adjacent lands)
+// 5. if not island or visited
+// -    go to next
+
 
 class Solution {
-    public void find_lands(int r, int c, boolean[][] visited, char[][] grid, int rlen, int clen) {
-        // visit 4 directions
+    public void find_lands(int r, int c, int rlen, int clen, boolean[][] visited, char[][] grid) {
+        // up, down, left, right
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         for(int[] direction : directions) {
             int nr = r + direction[0];
             int nc = c + direction[1];
-            // Valid: inside grid & '1' && not visited
-            if(nr >=0 && nr < rlen && nc >= 0 && nc < clen && grid[nr][nc] == '1' && !visited[nr][nc]) {
+            
+            // valid: not visited & '1' & inside the grid
+            if(nr >= 0 && nr < rlen && nc >= 0 && nc < clen && !visited[nr][nc] && grid[nr][nc] == '1') {
                 visited[nr][nc] = true;
-                // find adjacent of another island
-                find_lands(nr, nc, visited, grid, rlen, clen);
+                find_lands(nr, nc, rlen, clen, visited, grid);
             }
         }
     }
@@ -22,14 +31,14 @@ class Solution {
         boolean[][] visited = new boolean[rlen][clen];
         int count = 0;
 
-        // Iterate through every element in grid
-        for(int r=0; r<rlen; r++) {
-            for(int c=0; c<clen; c++) {
-                // '1' & not visited
+        // traverse every element in an array
+        for(int r=0; r < rlen; r++) {
+            for(int c=0; c < clen; c++) {
+                // if not visited and island
                 if(grid[r][c] == '1' && !visited[r][c]) {
-                    count++; // no of island
-                    visited[r][c] = true; // update to visited
-                    find_lands(r, c, visited, grid, rlen, clen);
+                    count++;
+                    visited[r][c] = true;
+                    find_lands(r, c, rlen, clen, visited, grid);
                 }
             }
         }
