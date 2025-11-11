b2ccf2012cde3f277b0f6f138e68e4ab475bd69c
Thinley <128626751+thinley4@users.noreply.github.com>
2025-11-11 10:59:36 +0530

    Time: 4 ms (29.38%) | Memory: 43.8 MB (5.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<>();
-        int n = s.length();
-        // iterate in string
-        for(int i=0; i<n; i++) {
-            // if stack is empty and close bracket return false
-            if(stk.empty() && ( s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']' ) )
-                return false;
-
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')  {
+        // 'open bracket' push
+        // 'close bracket' check top if match pop
+        for(int i=0; i<s.length(); i++) {
+            // open 
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
                 stk.push(s.charAt(i));
-            } else {
-            // for close bracket
-            // validate matching
-                if( (s.charAt(i) == ')' && stk.peek() == '(' ) || (s.charAt(i) == '}' && stk.peek()=='{' ) || (s.charAt(i) == ']' && stk.peek() == '[' ) ) {
+            else {
+                // close
+                // if stack empty invalid
+                if(stk.isEmpty())
+                    return false;
+
+                if( (stk.peek() == '(' && s.charAt(i) == ')') || (stk.peek() == '{' && s.charAt(i) == '}') || (stk.peek() == '[' && s.charAt(i) == ']') )
                     stk.pop();
-                } else {
-                    stk.push(s.charAt(i));
-                }
+                else
+                    return false;
             }
         }
 
-        return stk.empty();
+        return stk.isEmpty();
     }
 }
