13dfc18039003f9dc65b1def639398f7ae0a930f
Thinley <128626751+thinley4@users.noreply.github.com>
2025-11-11 11:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (6.01%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,14 +11,21 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        // single node
+        if(head == null || head.next == null)
+            return false;
+
         ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null ) {
+        ListNode fast = head.next;
+
+        while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
+
             if(slow == fast)
                 return true;
         }
+
         return false;
     }
 }
