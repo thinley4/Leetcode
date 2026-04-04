7c976d008b388a5672492501d7278f9740fb6dd4
Thinley <128626751+thinley4@users.noreply.github.com>
2026-04-04 12:48:47 +0530

    Time: 16 ms (82.88%) | Memory: 55.2 MB (45.40%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.java
@@ -0,0 +1,81 @@
+class Node {
+    int key;
+    int val;
+    Node next;
+
+    Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class MyHashMap {
+
+    //array of node
+    private Node[] map;
+
+    public MyHashMap() {
+        map = new Node[1000];
+        // storing -1 & -1 as key and value in map
+        for(int i=0; i<1000; i++) {
+            map[i] = new Node(-1, -1);
+        }
+    }
+    
+    public void put(int key, int value) {
+        int hash = hash(key);
+        Node cur = map[hash]; // cur = Node
+
+        while(cur.next != null) {
+            // if same key, update value
+            if(cur.next.key == key) {
+                cur.next.val = value;
+                return;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = new Node(key, value);
+    }
+    
+    public int get(int key) {
+        int hash = hash(key);
+        Node cur = map[hash].next;
+        
+        while(cur != null) {
+            if(cur.key == key) {
+                return cur.val;
+            }
+            cur = cur.next;
+        }
+
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int hash = hash(key);
+        Node cur = map[hash];
+
+        while(cur.next != null) {
+            if(cur.next.key == key) {
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
+    }
+
+    private int hash(int key) {
+        return key%1000;
+    }
+
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
