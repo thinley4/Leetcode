fcc76ebdb149ec4df7a0efae8048d7d771716a22
Thinley <128626751+thinley4@users.noreply.github.com>
2026-04-10 16:10:16 +0530

    Time: 3 ms (33.88%) | Memory: 48.7 MB (26.87%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,39 +1,41 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
-        if(digits == null || digits.length() == 0) {
+        HashMap<Character, char[]> map = new HashMap<>();
+
+        // no digit return empty array
+        if(digits.length() == 0)
             return res;
-        }
-        HashMap<Character, String> digitToLetters = new HashMap<>();
-        digitToLetters.put('2', "abc");
-        digitToLetters.put('3', "def");
-        digitToLetters.put('4', "ghi");
-        digitToLetters.put('5', "jkl");
-        digitToLetters.put('6', "mno");
-        digitToLetters.put('7', "pqrs");
-        digitToLetters.put('8', "tuv");
-        digitToLetters.put('9', "wxyz");
 
-        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
+        // Map digit to character
+        map.put('2', new char[]{'a','b','c'});
+        map.put('3', new char[]{'d','e','f'});
+        map.put('4', new char[]{'g','h','i'});
+        map.put('5', new char[]{'j','k','l'});
+        map.put('6', new char[]{'m','n','o'});
+        map.put('7', new char[]{'p','q','r','s'});
+        map.put('8', new char[]{'t','u','v'});
+        map.put('9', new char[]{'w','x','y','z'});
 
+        // call helper function
+        helper(digits, "", res, 0, map);
+        
         return res;
     }
 
-    public void backtrack(String digits, int idx, StringBuilder comb, List<String> res, HashMap<Character, String> digitToLetters) {
-        // if digits are two like "23" then when length equal to '2'
-        // then return
-        if(idx == digits.length()) {
-            res.add(comb.toString());
+    public void helper(String digits, String value, List<String> res, int index, HashMap<Character, char[]> map){
+        // aim
+        if(digits.length() == index) {
+            res.add(value);
             return;
         }
+        // 2 -> abc
+        char currDigit = digits.charAt(index); // 2
+        char[] charValues = map.get(currDigit); // [a, b, c]
 
-        String letters = digitToLetters.get(digits.charAt(idx));
-        // taking 'a' -> 'def'
-        // like: 'ad', 'ae', 'af'
-        for(char letter : letters.toCharArray()) {
-            comb.append(letter);
-            backtrack(digits, idx+1, comb, res, digitToLetters);
-            comb.deleteCharAt(comb.length() - 1);
+        for(int i=0; i<charValues.length; i++) {
+            helper(digits, value+charValues[i], res, index+1, map);
         }
+
     }
 }
